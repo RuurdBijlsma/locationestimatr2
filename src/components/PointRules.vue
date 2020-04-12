b69b0ac66471b132c234494b8b719324b4a1b5b4
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-04-12 15:14:38 +0200

    Fix custom rules page on mobile

--- src/components/PointRules.vue
+++ src/components/PointRules.vue
@@ -205,7 +205,8 @@
 
     @media screen and (max-width: 550px) {
         .rules-card {
-            height: 100%;
+            margin-bottom:60px;
+            min-height: 100%;
         }
     }
 
