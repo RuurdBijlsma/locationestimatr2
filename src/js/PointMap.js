9639bad52ca23adc77e58c47eec44553beb283d8
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-04-15 16:03:14 +0200

    Don't zoom to point map points

--- src/js/PointMap.js
+++ src/js/PointMap.js
@@ -12,11 +12,21 @@ export default class PointMap extends GeoMap {
                 position: new Google.maps.LatLng(...p.position)
             }
         });
+
+        this.worldBounds = [
+            [123.75000000000003, 66.79190947341792],
+            [97.73437500000004, -62.26792262941756],
+            [-53.65661087780617, -61.52386918380797],
+            [-123.74999999999996, -61.60639637138625],
+            [-111.79687499999996, 64.47279382008173],
+            [28.82812500000004, 65.0721300856071],
+        ].map(b => new Google.maps.LatLng(...b));
     }
 
     getBounds() {
         const bounds = new google.maps.LatLngBounds();
-        for (let {position} of this.googlePoints) {
+        // for (let {position} of this.googlePoints) {
+        for (let position of this.worldBounds) {
             bounds.extend(position);
         }
         return bounds;
